ee719e266316b63d072c60c2ddc893b97856fe58
Rakshith <hehaichi@gmail.com>
2020-12-01 10:24:44 -0500

    Add crypto module for RSA encryption

---
+++ zkSNARK/crypto.py
@@ -0,0 +1,101 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+import os
+import traceback
+
+def gen_keys():
+    private_key = rsa.generate_private_key(
+       public_exponent = 65537,
+       key_size = 2048,
+       backend = default_backend()
+    )
+    public_key = private_key.public_key()
+    return (public_key, private_key)
+
+def store_priv_key(private_key, filename):
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    with open(filename, "wb") as f:
+        f.write(pem)
+
+def store_pub_key(public_key, filename):
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    with open(filename, "wb") as f:
+        f.write(pem)
+
+def read_public_key(filename):
+    with open(filename, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+    return public_key
+
+def read_private_key(filename):
+    with open(filename, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+    return private_key
+
+def rsa_encrypt(message, public_key):
+    ciphertext = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return ciphertext
+
+def rsa_decrypt(ciphertext, private_key):
+    message = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return message
+
+def rsa_sign(message, signing_key):
+    signature = signing_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=20
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+def rsa_verify(message, signature, verification_key):
+    try:
+        verification_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=20
+            ),
+            hashes.SHA256()
+        )
+    except:
+        traceback.print_exc()
+        return False
+    else:
+        return True
